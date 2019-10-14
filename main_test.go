22e1dc506208ffc889c5d6bcdd5986f027ff5f34
Yuze Jiang <fortissimoX@gmail.com>
2019-10-14 14:08:12 -0500

    Test with both POST and GET

--- main_test.go
+++ main_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
+	"bytes"
 	"net/http"
 	"testing"
 	"time"
@@ -19,37 +19,36 @@ func TestConnection(t *testing.T) {
 func TestCreateClient(t *testing.T) {
 	go main()
 	time.Sleep(1 * time.Second)
-	resp, err := http.Get("http://127.0.0.1:8080/api/create-client")
+	url := "http://127.0.0.1:8080/api/honeyclient/create"
+	resp, err := http.Get(url)
 	if err != nil {
 		t.Error(err)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Error(err)
+	statusCode := resp.StatusCode
+	if statusCode != 405 {
+		t.Error("Expected http status code: 405, actual code: " + resp.Status)
 	}
 
-	response := string(body)
-	if len(response) == 0 {
-		t.Error("No response received")
+	var requestJson = []byte(`{"url": "https://www.google.com","screenshot": {"width": 1920,"height": 1080,"filename": "screenshot.png"},"useragent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15"}`)
+	resp, err = http.Post(url, "application/json", bytes.NewBuffer(requestJson))
+
+	statusCode = resp.StatusCode
+	if statusCode < 200 || statusCode > 299 {
+		t.Error("HTTP error, reason " + resp.Status)
 	}
 }
 
 func TestGetHoneyClientById(t *testing.T) {
 	go main()
 	time.Sleep(1 * time.Second)
-	resp, err := http.Get("http://127.0.0.1:8080/api/5")
+	resp, err := http.Get("http://127.0.0.1:8080/api/honeyclient/5")
 	if err != nil {
 		t.Error(err)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Error(err)
-	}
-
-	response := string(body)
-	if len(response) == 0 {
-		t.Error("No response received")
+	statusCode := resp.StatusCode
+	if statusCode < 200 || statusCode > 299 {
+		t.Error("HTTP error, reason " + resp.Status)
 	}
 }
