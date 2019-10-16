7e9dd6dc0b753fefc7e228f6a14d78d62f526cf7
William Teder <wteder@hydreon.com>
2019-10-16 00:26:23 -0500

    refactor(http): format responses as JSON

--- routes/create_honey_client.go
+++ routes/create_honey_client.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/csci4950tgt/api/models"
-	"net/http"
+	"github.com/csci4950tgt/api/util"
 	"io/ioutil"
+	"net/http"
 )
 
 func SetupCreateResponse(w *http.ResponseWriter, r *http.Request) {
@@ -19,8 +20,8 @@ func CreateHoneyClient(w http.ResponseWriter, r *http.Request) {
 	SetupCreateResponse(&w, r)
 	// If request type is not current request, return error.
 	if (*r).Method != "POST" {
-		w.WriteHeader(405)
-		fmt.Fprintf(w, "Method \"%s\" is not allowed.\n",(*r).Method)
+		util.WriteHttpErrorCode(w, http.StatusMethodNotAllowed, fmt.Sprintf("Method \"%s\" is not allowed.\n", (*r).Method))
+
 		return
 	}
 
@@ -37,11 +38,23 @@ func CreateHoneyClient(w http.ResponseWriter, r *http.Request) {
 	file_name := fmt.Sprintf("../honeyclient/input/%d.json", ticket.ID)
 	file, _ := json.MarshalIndent(ticket, "", " ")
 	err := ioutil.WriteFile(file_name, file, 0755)
+
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "Error happens in connecting honeyclient. %s.\n",
-					err.Error())
-	} else {
-		fmt.Fprintf(w, "Create ticket '%s' with ID '%d' and URL '%s'", ticket.Name, ticket.ID, ticket.URL)
+		util.WriteHttpErrorCode(w, http.StatusInternalServerError, "Failed to write file for honeyclient to consume.")
+
+		fmt.Println("Failed to write file for honeyclient to consume:")
+		fmt.Println(err)
+
+		return
 	}
+
+	// TODO: run honeyclient and return error if failed
+
+	msg := fmt.Sprintf("Create ticket '%s' with ID '%d' and URL '%s'", ticket.Name, ticket.ID, ticket.URL)
+	res := models.Response{
+		Success: true,
+		Message: &msg,
+	}
+
+	util.WriteHttpResponse(w, res)
 }
--- routes/get_honey_client_by_id.go
+++ routes/get_honey_client_by_id.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"fmt"
 	"github.com/csci4950tgt/api/models"
+	"github.com/csci4950tgt/api/util"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
@@ -19,13 +20,20 @@ func GetHoneyClientById(w http.ResponseWriter, r *http.Request) {
 	SetupGetResponse(&w, r)
 	// If request type is not current request, return error.
 	if (*r).Method != "GET" {
-		w.WriteHeader(405)
-		fmt.Fprintf(w, "Method \"%s\" is not allowed.\n",(*r).Method)
+		util.WriteHttpErrorCode(w, http.StatusMethodNotAllowed, fmt.Sprintf("Method \"%s\" is not allowed.\n", (*r).Method))
+
 		return
 	}
 
 	vars := mux.Vars(r)               // get dynamic variables from mux handler
 	id, _ := strconv.Atoi(vars["id"]) // get integer "ID" from vars
 	ticket := models.Ticket{ID: id, Name: "Hardcoded ticket", URL: "https://example.com"}
-	fmt.Fprintf(w, "Fetched ticket '%s' from ID '%d' with URL '%s'", ticket.Name, ticket.ID, ticket.URL)
+
+	msg := fmt.Sprintf("Fetched ticket '%s' from ID '%d' with URL '%s'", ticket.Name, ticket.ID, ticket.URL)
+	res := models.Response{
+		Success: true,
+		Message: &msg,
+	}
+
+	util.WriteHttpResponse(w, res)
 }
