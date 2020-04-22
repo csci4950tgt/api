2f800128f5e8693387d9555c3495ff47909ba006
jin-yng <alexjohn.choi@gmail.com>
2020-04-22 11:47:38 -0500

    test(dockertest): setup a dockertest env

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/csci4950tgt/api
 go 1.13
 
 require (
+	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/jinzhu/gorm v1.9.11
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/lib/pq v1.2.0
+	github.com/ory/dockertest/v3 v3.6.0
 )
--- models/db_test.go
+++ models/db_test.go
@@ -1,16 +1,52 @@
 package models
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"log"
 	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/ory/dockertest/v3"
 )
+
+var testDb *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := pool.Run("postgres", "9.6", []string{"POSTGRES_PASSWORD=testing", "POSTGRES_DB=testing"})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+	// set expiration to avoid dead instances
+	resource.Expire(30)
+
+	if err = pool.Retry(func() error {
+		var err error
+		testDb, err = sql.Open("postgres", fmt.Sprintf("postgres://postgres:secret@localhost:%s/%s?sslmode=disable", resource.GetPort("5432/tcp"), database))
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// When you're done, kill and remove the container
+	err = pool.Purge(resource)
+}
 
 func TestInitDB(t *testing.T) {
 	ConnectToTesting()
 }
 
 func ConnectToTesting() {
-	InitDB("host=127.0.0.1 port=5432 user=testing dbname=testing password=testing sslmode=disable")
-	ResetTables()
 }
 
 func ResetTables() {
@@ -41,4 +77,5 @@ func ResetTables() {
 	for _, t := range tickets {
 		db.Create(&t)
 	}
+
 }
